59c7c1af5ff173341522ef92244484c027c53c24
muhammadshiva <shivamuhammad07@gmail.com>
2022-06-14 17:49:18 +0700

    Add widgets

---
+++ lib/widgets/space_card.dart
@@ -0,0 +1,109 @@
+import 'package:find_house_app/pages/detail_page.dart';
+import 'package:flutter/material.dart';
+import 'package:find_house_app/theme.dart';
+import '../models/space.dart';
+
+class SpaceCard extends StatelessWidget {
+  final Space space;
+  SpaceCard(this.space);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DetailPage()),
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    space.imageUrl,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: purpleColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(36),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon_star.png',
+                            width: 22,
+                            height: 22,
+                          ),
+                          Text(
+                            '${space.rating}/5',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                space.name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: '\$${space.price}',
+                  style: purpleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: ' / month',
+                      style: greyTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${space.city}, ${space.country}',
+                style: greyTextStyle,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
